1fd8af0073826c546e62a05c95b3562b406c4ed5
Liban Hussein <libanhussein11@hotmail.com>
2024-10-17 01:35:02 +0200

    Fixed the animation for waving hand

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,10 +3,11 @@ import {
     Stack,
     Container,
     Heading,
-    Text,
+    Text
   } from '@chakra-ui/react'
 
 import { extendTheme } from '@chakra-ui/react'
+import '../App.css'
 
   const theme = extendTheme({
     fonts: {
@@ -19,9 +20,10 @@ function About() {
   return (
     <>
     <ChakraProvider theme={theme}>
-        <Stack>
+        <Stack spacing={'100px'}>
             <Container maxW='container.lg' mt={30} mb={30}>
-                <Heading fontWeight={'bold'} fontSize={'5xl'}>Welcome!👋</Heading>
+                <Heading fontWeight={'bold'} fontSize={'5xl'}>Welcome! <span className='handEmoji'
+                >👋</span></Heading>
                 <Text>I'm a passionate Microsoft 365 & Frontend Developer with a knack for transforming innovative ideas into reality. With expertise in SharePoint, Power Platform, and Teams, I specialize in creating seamless digital experiences that enhance productivity and collaboration. Driven by a love for technology and continuous learning, I aim to empower organizations to harness the full potential of Microsoft 365 solutions. Let's build the future together! 🚀</Text>
             </Container>
         </Stack>
